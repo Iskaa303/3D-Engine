f6a8117183f0fde74febed9f7199d3db8c7daedb
Iskaa303 <iska060808@gmail.com>
2024-06-07 19:10:27 -0400

    index and staging buffers

--- src/model.cpp
+++ src/model.cpp
@@ -6,40 +6,106 @@
 
 namespace Engine {
 
-Model::Model(Device &device, const std::vector<Vertex> &vertices) : engineDevice{device} {
-    createVertexBuffers(vertices);
+Model::Model(Device &device, const Model::Builder &builder) : engineDevice{device} {
+    createVertexBuffers(builder.vertices);
+    createIndexBuffers(builder.indices);
 }
 
 Model::~Model() {
     vkDestroyBuffer(engineDevice.device(), vertexBuffer, nullptr);
     vkFreeMemory(engineDevice.device(), vertexBufferMemory, nullptr);
+
+    if (hasIndexBuffer) {
+        vkDestroyBuffer(engineDevice.device(), indexBuffer, nullptr);
+        vkFreeMemory(engineDevice.device(), indexBufferMemory, nullptr);
+    }
 }
 
 void Model::createVertexBuffers(const std::vector<Vertex> &vertices) {
     vertexCount = static_cast<uint32_t>(vertices.size());
     assert(vertexCount >= 3 && "Vertex count must be at least 3");
     VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
     engineDevice.createBuffer(
         bufferSize,
-        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
         VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        stagingBuffer,
+        stagingBufferMemory);
+
+    void *data;
+    vkMapMemory(engineDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+    vkUnmapMemory(engineDevice.device(), stagingBufferMemory);
+
+    engineDevice.createBuffer(
+        bufferSize,
+        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
         vertexBuffer,
         vertexBufferMemory);
 
+    engineDevice.copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+    vkDestroyBuffer(engineDevice.device(), stagingBuffer, nullptr);
+    vkFreeMemory(engineDevice.device(), stagingBufferMemory, nullptr);
+}
+
+void Model::createIndexBuffers(const std::vector<uint32_t> &indices) {
+    indexCount = static_cast<uint32_t>(indices.size());
+    hasIndexBuffer = indexCount > 0;
+
+    if (!hasIndexBuffer) {
+        return;
+    }
+
+    VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    engineDevice.createBuffer(
+        bufferSize,
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+        stagingBuffer,
+        stagingBufferMemory);
+
     void *data;
-    vkMapMemory(engineDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
-    memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-    vkUnmapMemory(engineDevice.device(), vertexBufferMemory);
+    vkMapMemory(engineDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+    memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+    vkUnmapMemory(engineDevice.device(), stagingBufferMemory);
+
+    engineDevice.createBuffer(
+        bufferSize,
+        VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+        VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+        indexBuffer,
+        indexBufferMemory);
+
+    engineDevice.copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+    vkDestroyBuffer(engineDevice.device(), stagingBuffer, nullptr);
+    vkFreeMemory(engineDevice.device(), stagingBufferMemory, nullptr);
 }
 
 void Model::draw(VkCommandBuffer commandBuffer) {
-    vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+    if (hasIndexBuffer) {
+        vkCmdDrawIndexed(commandBuffer, indexCount, 1, 0, 0, 0);
+    } else {
+        vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+    }
 }
 
 void Model::bind(VkCommandBuffer commandBuffer) {
     VkBuffer buffers[] = {vertexBuffer};
     VkDeviceSize offsets[] = {0};
     vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+
+    if (hasIndexBuffer) {
+        vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT32);
+    }
 }
 
 std::vector<VkVertexInputBindingDescription> Model::Vertex::getBindingDescriptions() {
--- src/model.hpp
+++ src/model.hpp
@@ -21,7 +21,12 @@ class Model {
             static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
         };
 
-        Model(Device &device, const std::vector<Vertex> &vertices);
+        struct Builder {
+            std::vector<Vertex> vertices{};
+            std::vector<uint32_t> indices{};
+        };
+
+        Model(Device &device, const Model::Builder &builder);
         ~Model();
 
         Model(const Model &) = delete;
@@ -32,10 +37,17 @@ class Model {
 
     private:
         void createVertexBuffers(const std::vector<Vertex> &vertices);
+        void createIndexBuffers(const std::vector<uint32_t> &indices);
 
         Device &engineDevice;
+
         VkBuffer vertexBuffer;
         VkDeviceMemory vertexBufferMemory;
         uint32_t vertexCount;
+
+        bool hasIndexBuffer = false;
+        VkBuffer indexBuffer;
+        VkDeviceMemory indexBufferMemory;
+        uint32_t indexCount;
 };
 }  // namespace Engine
