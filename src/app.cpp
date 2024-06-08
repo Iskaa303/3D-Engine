c74e4ec9019148707f2309c983c4f680a68bea0a
Iskaa303 <iska060808@gmail.com>
2024-06-07 20:05:59 -0400

    small code reformatting

--- src/app.cpp
+++ src/app.cpp
@@ -49,12 +49,12 @@ void App::run() {
         camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 10.f);
 
         if (auto commandBuffer = engineRenderer.beginFrame()) {
-        engineRenderer.beginSwapChainRenderPass(commandBuffer);
+            engineRenderer.beginSwapChainRenderPass(commandBuffer);
 
-        simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects, camera);
+            simpleRenderSystem.renderGameObjects(commandBuffer, gameObjects, camera);
 
-        engineRenderer.endSwapChainRenderPass(commandBuffer);
-        engineRenderer.endFrame();
+            engineRenderer.endSwapChainRenderPass(commandBuffer);
+            engineRenderer.endFrame();
         }
     }
     vkDeviceWaitIdle(engineDevice.device());
@@ -62,59 +62,59 @@ void App::run() {
 
 // temporary helper function, creates a 1x1x1 cube centered at offset with an index buffer
 std::unique_ptr<Model> createCubeModel(Device& device, glm::vec3 offset) {
-      Model::Builder modelBuilder{};
-      modelBuilder.vertices = {
-      // left face (white)
-      {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
-      {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
-      {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
-      {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+    Model::Builder modelBuilder{};
+    modelBuilder.vertices = {
+    // left face (white)
+    {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+    {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+    {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+    {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
 
-      // right face (yellow)
-      {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
-      {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
-      {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
-      {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+    // right face (yellow)
+    {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+    {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+    {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+    {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
 
-      // top face (orange, remember y axis points down)
-      {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-      {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-      {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-      {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+    // top face (orange, remember y axis points down)
+    {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+    {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+    {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+    {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
 
-      // bottom face (red)
-      {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-      {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
-      {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
-      {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+    // bottom face (red)
+    {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+    {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+    {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+    {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
 
-      // nose face (blue)
-      {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-      {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-      {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-      {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+    // nose face (blue)
+    {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+    {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+    {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+    {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
 
-      // tail face (green)
-      {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-      {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-      {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-      {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-  };
-  for (auto& v : modelBuilder.vertices) {
-    v.position += offset;
-  }
-  modelBuilder.indices = {0,  1,  2,  0,  3,  1,  4,  5,  6,  4,  7,  5,  8,  9,  10, 8,  11, 9,
-                          12, 13, 14, 12, 15, 13, 16, 17, 18, 16, 19, 17, 20, 21, 22, 20, 23, 21};
+    // tail face (green)
+    {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+    {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+    {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+    {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+    };
+    for (auto& v : modelBuilder.vertices) {
+        v.position += offset;
+    }
+    modelBuilder.indices = {0,  1,  2,  0,  3,  1,  4,  5,  6,  4,  7,  5,  8,  9,  10, 8,  11, 9,
+                            12, 13, 14, 12, 15, 13, 16, 17, 18, 16, 19, 17, 20, 21, 22, 20, 23, 21};
 
-  return std::make_unique<Model>(device, modelBuilder);
+    return std::make_unique<Model>(device, modelBuilder);
 }
 
 void App::loadGameObjects() {
-  std::shared_ptr<Model> engineModel = createCubeModel(engineDevice, {.0f, .0f, .0f});
-  auto cube = GameObject::createGameObject();
-  cube.model = engineModel;
-  cube.transform.translation = {.0f, .0f, .5f};
-  cube.transform.scale = {.5f, .5f, .5f};
-  gameObjects.push_back(std::move(cube));
+    std::shared_ptr<Model> engineModel = createCubeModel(engineDevice, {.0f, .0f, .0f});
+    auto cube = GameObject::createGameObject();
+    cube.model = engineModel;
+    cube.transform.translation = {.0f, .0f, .5f};
+    cube.transform.scale = {.5f, .5f, .5f};
+    gameObjects.push_back(std::move(cube));
 }
-}  // namespace Engine
+} // namespace Engine
--- src/systems/render_system.cpp
+++ src/systems/render_system.cpp
@@ -12,77 +12,76 @@
 #include <stdexcept>
 
 namespace Engine {
-
+    
 struct SimplePushConstantData {
-  glm::mat4 transform{1.f};
-  alignas(16) glm::vec3 color{};
+    glm::mat4 transform{1.f};
+    alignas(16) glm::vec3 color{};
 };
 
 RenderSystem::RenderSystem(Device& device, VkRenderPass renderPass)
-    : engineDevice{device} {
-  createPipelineLayout();
-  createPipeline(renderPass);
+        : engineDevice{device} {
+    createPipelineLayout();
+    createPipeline(renderPass);
 }
 
 RenderSystem::~RenderSystem() {
-  vkDestroyPipelineLayout(engineDevice.device(), pipelineLayout, nullptr);
+    vkDestroyPipelineLayout(engineDevice.device(), pipelineLayout, nullptr);
 }
 
 void RenderSystem::createPipelineLayout() {
-  VkPushConstantRange pushConstantRange{};
-  pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
-  pushConstantRange.offset = 0;
-  pushConstantRange.size = sizeof(SimplePushConstantData);
+    VkPushConstantRange pushConstantRange{};
+    pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+    pushConstantRange.offset = 0;
+    pushConstantRange.size = sizeof(SimplePushConstantData);
 
-  VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
-  pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-  pipelineLayoutInfo.setLayoutCount = 0;
-  pipelineLayoutInfo.pSetLayouts = nullptr;
-  pipelineLayoutInfo.pushConstantRangeCount = 1;
-  pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
-  if (vkCreatePipelineLayout(engineDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) !=
-      VK_SUCCESS) {
-    throw std::runtime_error("failed to create pipeline layout!");
-  }
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.setLayoutCount = 0;
+    pipelineLayoutInfo.pSetLayouts = nullptr;
+    pipelineLayoutInfo.pushConstantRangeCount = 1;
+    pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
+    if (vkCreatePipelineLayout(engineDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) !=
+            VK_SUCCESS) {
+        throw std::runtime_error("failed to create pipeline layout!");
+    }
 }
 
 void RenderSystem::createPipeline(VkRenderPass renderPass) {
-  assert(pipelineLayout != nullptr && "Cannot create pipeline before pipeline layout");
+    assert(pipelineLayout != nullptr && "Cannot create pipeline before pipeline layout");
 
-  PipelineConfigInfo pipelineConfig{};
-  Pipeline::defaultPipelineConfigInfo(pipelineConfig);
-  pipelineConfig.renderPass = renderPass;
-  pipelineConfig.pipelineLayout = pipelineLayout;
-  enginePipeline = std::make_unique<Pipeline>(
-      engineDevice,
-      "shaders/compiled/shader.vert.spv",
-      "shaders/compiled/shader.frag.spv",
-      pipelineConfig);
+    PipelineConfigInfo pipelineConfig{};
+    Pipeline::defaultPipelineConfigInfo(pipelineConfig);
+    pipelineConfig.renderPass = renderPass;
+    pipelineConfig.pipelineLayout = pipelineLayout;
+    enginePipeline = std::make_unique<Pipeline>(
+        engineDevice,
+        "shaders/compiled/shader.vert.spv",
+        "shaders/compiled/shader.frag.spv",
+        pipelineConfig);
 }
 
 void RenderSystem::renderGameObjects(
-    VkCommandBuffer commandBuffer,
-    std::vector<GameObject>& gameObjects,
-    const Camera& camera) {
-  enginePipeline->bind(commandBuffer);
+        VkCommandBuffer commandBuffer,
+        std::vector<GameObject>& gameObjects,
+        const Camera& camera) {
+    enginePipeline->bind(commandBuffer);
 
-  auto projectionView = camera.getProjection() * camera.getView();
+    auto projectionView = camera.getProjection() * camera.getView();
 
-  for (auto& obj : gameObjects) {
-    SimplePushConstantData push{};
-    push.color = obj.color;
-    push.transform = projectionView * obj.transform.mat4();
+    for (auto& obj : gameObjects) {
+        SimplePushConstantData push{};
+        push.color = obj.color;
+        push.transform = projectionView * obj.transform.mat4();
 
-    vkCmdPushConstants(
-        commandBuffer,
-        pipelineLayout,
-        VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
-        0,
-        sizeof(SimplePushConstantData),
-        &push);
-    obj.model->bind(commandBuffer);
-    obj.model->draw(commandBuffer);
-  }
+        vkCmdPushConstants(
+            commandBuffer,
+            pipelineLayout,
+            VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+            0,
+            sizeof(SimplePushConstantData),
+            &push);
+        obj.model->bind(commandBuffer);
+        obj.model->draw(commandBuffer);
+    }
 }
-
-}  // namespace Engine
+} // namespace Engine
